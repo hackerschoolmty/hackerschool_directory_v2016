fa9a05b0bf9de17933707a443f870b8888283409
Mayra Cabrera <mcabrera1087@gmail.com>
2016-02-13 12:52:32 -0600

    Add before_action to hackers controller
    This was because rails rules. To see before_action description go to rails guides

--- app/controllers/hackers_controller.rb
+++ app/controllers/hackers_controller.rb
@@ -1,36 +1,32 @@
 class HackersController < ApplicationController
+  before_action :find_generation
 
 
   # GET /hackers
   # GET /hackers.json
   def index
-    @generation = Generation.find(params[:generation_id])
     @hackers = @generation.hackers
   end
 
   # GET /hackers/1
   # GET /hackers/1.json
   def show
-    @generation = Generation.find(params[:generation_id])
     @hacker = @generation.hackers.find(params[:id])
   end
 
   # GET /hackers/new
   def new
-    @generation = Generation.find(params[:generation_id])
     @hacker = @generation.hackers.new
   end
 
   # GET /hackers/1/edit
   def edit
-    @generation = Generation.find(params[:generation_id])
     @hacker = @generation.hackers.find(params[:id])
   end
 
   # POST /hackers
   # POST /hackers.json
   def create
-    @generation = Generation.find(params[:generation_id])
     @hacker = @generation.hackers.new(hacker_params)
 
     respond_to do |format|
@@ -47,7 +43,6 @@ class HackersController < ApplicationController
   # PATCH/PUT /hackers/1
   # PATCH/PUT /hackers/1.json
   def update
-    @generation = Generation.find(params[:generation_id])
     @hacker = @generation.hackers.find(params[:id])
     respond_to do |format|
       if @hacker.update(hacker_params)
@@ -63,7 +58,6 @@ class HackersController < ApplicationController
   # DELETE /hackers/1
   # DELETE /hackers/1.json
   def destroy
-    @generation = Generation.find(params[:generation_id])
     @hacker = @generation.hackers.find(params[:id])
     @hacker.destroy
     respond_to do |format|
@@ -79,4 +73,13 @@ class HackersController < ApplicationController
     def hacker_params
       params.require(:hacker).permit(:name, :email, :generation_id, language_ids: [])
     end
+
+    def find_generation
+      @generation = Generation.find(params[:generation_id])
+    end
+
+    def watch_out
+      binding.pry
+    end
+
 end
