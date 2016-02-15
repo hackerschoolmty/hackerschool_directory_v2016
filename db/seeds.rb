c491f4a80f9518eb70f17f245f4f99306d6b2fd6
walreyes <wal.reyes.11@gmail.com>
2016-02-14 23:04:03 -0600

    Seeds database
    Generates 10 languages
    Generates 5 generations
    each generation has 3 hackers with 5 languages and 5 courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Seed languages
+10.times do
+
+	language = Language.create({name: Faker::Hacker.adjective})
+
+end
+
+5.times do
+
+	#Create generation
+	generation = Generation.create({name: Faker::App.name, start_date: Faker::Date.backward(10)})
+
+	#Create hackers
+	3.times do
+		hacker = generation.hackers.create({name: Faker::Name.name, email: Faker::Internet.email, github_account: Faker::Internet.user_name})
+		
+		#Por qué esto no funciona?
+		#hacker.language_ids = Language.pluck(:id).shuffle[0..4]
+		
+		#Y esto sí?
+		hacker.language_ids = Language.pluck(:id).shuffle[0..4]
+
+	end
+
+	#Create courses
+	5.times do
+		course = generation.courses.create({name: Faker::App.name})
+	end
+
+	
+end
