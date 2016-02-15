d73ae506ef9e4122ad1568fdd3e9a996180095ab
walreyes <wal.reyes.11@gmail.com>
2016-02-14 21:51:52 -0600

    Adds validations to hacker
    Name must be presence
    Email and github accoutn must be unique

--- app/models/hacker.rb
+++ app/models/hacker.rb
@@ -1,4 +1,7 @@
 class Hacker < ActiveRecord::Base
   belongs_to :generation
   has_and_belongs_to_many :languages
+
+  validates :name, presence: true
+  validates :email, :github_account, uniqueness: true
 end
